180dfc30358ff3c1eacf36f4bb823972358a43c2
pdparchitect <pdp@gnucitizen.org>
2024-09-02 15:24:18 +0000

    First.

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "flag"
+    "log"
+    "net/http"
+    "net/http/httputil"
+    "net/url"
+)
+
+func main() {
+    var (
+        target string
+        port   string
+    )
+
+    flag.StringVar(&target, "url", "", "The URL to forward requests to")
+    flag.StringVar(&port, "port", "8080", "The port to run the proxy server on")
+
+	flag.Parse()
+
+    if target == "" {
+        log.Fatal("You must specify a URL with the -url flag.")
+    }
+
+    targetURL, err := url.Parse(target)
+    if err != nil {
+        log.Fatalf("Failed to parse target URL: %s", err)
+    }
+
+    proxy := httputil.NewSingleHostReverseProxy(targetURL)
+    http.Handle("/", proxy)
+
+    log.Printf("Starting proxy server on port %s forwarding to %s", port, target)
+    log.Fatal(http.ListenAndServe(":"+port, nil))
+}
